2e0d8bb31bebf6bcf1105b9626ae6a3df97b4ec2
DevashishJoshi <deva.neggi@gmail.com>
2017-10-27 10:15:30 +0000

    Update gradientDesc.py

--- LogisticRegWs/gradientDesc.py
+++ LogisticRegWs/gradientDesc.py
@@ -6,8 +6,6 @@ def gradDesc(theta, X, y, alpha, num_iters, lambda_):
 	m = y.shape[0]
 	n = X.shape[1]
 	j_history = np.zeros( (num_iters) )
-	k=1
-	prev_rate=0
 	for i in range(0, num_iters):
 		theta = theta - np.array( alpha*grad(theta, X, y, lambda_) ).T
 		j_history[i] = cost(theta, X, y, lambda_)
